de305953cec421c68172b1430ee9fb06a74fb37e
fade222 <fade222@users.noreply.github.com>
2019-10-16 00:02:45 +0900

    fix: add high and low forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (longitude, latitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. With a high of ${body.daily.data[0].temperatureHigh} and a low of ${body.daily.data[0].temperatureLow}.`
       );
     }
   });
